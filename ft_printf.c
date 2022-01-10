5ff2193988b241641e3c9a1dff9453ddda13efac
Vincent Mercier <vincent.mrc@hotmail.com>
2022-01-10 12:01:55 +0100

    Algo for ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: vimercie <vimercie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 17:23:49 by vimercie          #+#    #+#             */
-/*   Updated: 2021/12/21 17:58:17 by vimercie         ###   ########lyon.fr   */
+/*   Updated: 2022/01/10 12:00:04 by vimercie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,31 @@
 
 int	ft_printf(const char *format, ...)
 {
-	
+	size_t	len;
+	size_t	i;
+	va_list	ap;
+
+	len = ft_strlen(format);
+	i = 0;
+	va_start(ap, format);
+	while (format[i] != '%' && i != len)
+	{
+		write(1, format[i], 1);
+		i++;
+	}
+	if (i != len)
+		if_forest(format[i + 1], va_arg());
+}
+
+char	*if_forest(char type)
+{
+	if (type == c)
+	if (type == s)
+	if (type == p)
+	if (type == d)
+	if (type == i)
+	if (type == u)
+	if (type == x)
+	if (type == X)
+	if (type == %)
 }
